39b8c4660b205e33e6dbc9581ccaabe561fd298d
Sulaiman Baig <engrsulaimanbaig@gmail.com>
2022-08-23 13:18:02 +0500

    "mongoose": "6.2.10",

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var db = 'mongodb+srv://user:cruduser@cluster0.u9l50pz.mongodb.net/?retryWrites=
 
 var books = require('./routes/books');
 
-mongoose.connect(db, { useNewUrlParser: true })
+mongoose.connect(db)
 // .then((connected) => {
 //     console.log(`connected to mongodb`);
 app.use(bodyParser.json());
